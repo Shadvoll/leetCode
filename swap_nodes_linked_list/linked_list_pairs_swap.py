d1dd53b8a9bf92c61eacf7643554d4f37d441efe
shaukat <shadvoll@gmail.com>
2023-05-24 22:26:41 +0300

    added python solution 'linked list swap pair nodes'

--- swap_nodes_linked_list/linked_list_pairs_swap.py
+++ swap_nodes_linked_list/linked_list_pairs_swap.py
@@ -3,15 +3,34 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-    def showLinkedList(self):
-        while self is not None:
-            print(self.val,end=' ')
-            self = self.next
+
+
 class Solution:
-    def pairSum(self, head: ListNode) -> int:
-        ...
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if head is None or head.next is None:
+            return head
+        currentNode = head.next
+        nextNode = currentNode.next
+        currentNode.next = head
+        head.next = self.swapPairs(nextNode)
+        return currentNode
+        
+
+    @staticmethod
+    def showList(head: ListNode):
+        iterator = head
+        while iterator is not None:
+            print(iterator.val, end=' ')
+            iterator = iterator.next
+        print()
+        
+            
 
 
-if __name__ == "__main__":
-    linked_list = ListNode(10,ListNode(3,ListNode(1,ListNode(4))))
-    linked_list.showLinkedList()
+if __name__ == '__main__':
+
+    my_lst = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
+
+    Solution.showList(my_lst)
+    my_lst = Solution().swapPairs(my_lst)
+    Solution.showList(my_lst)
